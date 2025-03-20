a2874419d045a106bd1f6648c04f6dd26b786b06
MeenalSoni13794 <minalsoni006@gmail.com>
2025-03-20 17:30:14 +0000

    Adding model level contraints and flipper gem

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
-end
+    # Adding validation
+    validates :title, presence: true, uniqueness: true, length: { maximum: 100 }
+    validates :description, presence: true, length: { maximum: 500 }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  end
